8bbbe4cf79d6c63436c3db29e1e48a75fd975d96
Alexandre Terrasa <alexandre@moz-code.org>
2014-11-11 19:44:36 -0500

    add HTML catalog views and routes
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/catalog');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Online Catalog' });
+});
+
+/* GET albums page. */
+router.get('/albums', function(req, res) {
+	res.render('albums', {catalog: data.catalog});
+});
+
+/* GET album page. */
+router.get('/album/:id', function(req, res) {
+	var id = req.params.id;
+	res.render('album', data.catalog[id]);
 });
 
 module.exports = router;
